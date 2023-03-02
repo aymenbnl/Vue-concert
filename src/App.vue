3178bb4495f5d8c62f2543b4d7ad964472d08854
louis <louisdosnon@gmail.com>
2023-03-02 19:12:25 +0100

    ajout connection utilisateur et debut connection administrateur

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,73 @@
 				onglet: "groupes",
 				modeConnecte: false,
 				user: {},
+        utilisateurs: [],
+        admin: [],
 			}
 		},
-
 		methods: {
 			handleMenuClick(nomOnglet) {
 				this.onglet = nomOnglet
-			}
-		}
+			},
+      handleIncription() {
+        let user = {
+          identifiant : this.ident,
+          motPasse : this.pwd
+        }
+        console.log(JSON.stringify(user))
+        fetch ("http://localhost:8080/concert_site_war_exploded/ticket-api/utilisateurs", {
+          method: 'POST',
+          mode: 'no-cors',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(user)
+        })
+        this.getData()
+      },
+      handleConnection() {
+        console.log(this.ident)
+        console.log(this.pwd)
+        console.log(this.utilisateurs)
+        this.utilisateurs.map((utilisateur) => {
+          console.log(utilisateur)
+          if (utilisateur.identifiant === this.ident && utilisateur.motPasse === this.pwd) {
+            console.log("connection ok")
+            this.user = utilisateur;
+            this.modeConnecte = true;
+          }
+        })
+      },
+      handleCoAdmin() {
+        let user = {
+          login : this.ident,
+          motDePasse : this.pwd
+        }
+        console.log(user)
+        fetch ("http://localhost:8080/concert_site_war_exploded/admin-api/admins/1", {
+          method: 'GET',
+          mode: 'no-cors',
+        })
+            .then((response) => {
+              console.log(response.status)
+            })
+            .catch(error => console.error("err fetch: $(error.message)"))
+      },
+      handleDeconnection() {
+        this.user = {};
+        this.modeConnecte = false;
+      },
+      getData() {
+        fetch("http://localhost:8080/concert_site_war_exploded/ticket-api/utilisateurs")
+            .then((response) => response.json())
+            .then((utilisateurs) => {
+              this.utilisateurs = utilisateurs;
+            });
+      }
+		},
+    created() {
+      this.getData();
+    }
 	}
 
 </script>
@@ -42,10 +101,14 @@
 				<div v-if="modeConnecte">
 					<p v-if="user==={}">Administrateur</p>
 					<p v-else>{{user.identifiant}}</p>
+          <button id="bt_deco" @click="handleDeconnection">se deconnecter</button>
 				</div>
 				<div v-else>
-					<button id="bt-co">Se connecter</button>
-					<button>S'inscrire</button>
+          <input type="text" id="ident" v-model="ident"/>
+          <input type="text" id="pwd" v-model="pwd"/>
+					<button id="bt-co" @click="handleConnection">Se connecter</button>
+					<button id="bt_ins" @click="handleIncription">S'inscrire</button>
+          <button id="bt_co_admin" @click="handleCoAdmin">se connecter en administrateur</button>
 				</div>
 			</div>
 
