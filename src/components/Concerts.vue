a63f589b8a5c1a3ffb9b0dd6d29ef134045c04eb
louis <louisdosnon@gmail.com>
2023-03-02 11:49:06 +0100

    ajout add et del de concerts

--- src/components/Concerts.vue
+++ src/components/Concerts.vue
@@ -3,6 +3,7 @@
   <table class="table">
     <thead>
       <tr>
+        <th scope="col">id</th>
         <th scope="col">Date</th>
         <th scope="col">Heure de debut</th>
         <th scope="col">Heure de fin</th>
@@ -10,12 +11,15 @@
     </thead>
     <tbody v-for="c in propsConcerts">
       <tr>
+        <td>{{c.id_concert}}</td>
         <td>{{c.date}}</td>
         <td>{{c.heureDebut}}</td>
         <td>{{c.heureFin}}</td>
+        <td><a href="#" @click="this.handleDelConcert(c.id_concert)">del</a></td>
       </tr>
     </tbody>
   </table>
+  <p><a href="#" @click="this.handleAddConcert()">add</a></p>
 </template>
 
 <script>
@@ -25,7 +29,30 @@
           propsModeConnecte: Boolean,
           propsUser: Object
         },
-
+        methods: {
+          handleDelConcert(id) {
+            fetch ("http://localhost:3001/concerts/"+id, {
+              method: 'DELETE',
+            })
+          },
+          handleAddConcert() {
+            fetch ("http://localhost:3001/concerts", {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                "date": "2023-03-02T23:00:00.000+00:00",
+                "heureDebut": "13:00:00",
+                "heureFin": "14:30:00",
+                "id_groupe": 1,
+                "soiree": {
+                  "id_soiree": 1
+                }
+              })
+            })
+          }
+        }
     }
 </script>
 
